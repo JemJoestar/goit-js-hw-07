db67b59c914ed25bb2af76f376949a4d1d2863f1
JemJoestar <jemapro2@gmail.com>
2023-07-08 13:12:58 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery")
+let currentShowingPhoto
+
+
+// ~Функція для створення елементів галереї
+function createGalleryItem({preview:miniUrl, original:url, description:alt}){
+    const newItem = document.createElement("li")
+    newItem.insertAdjacentHTML("afterbegin",`<a class="gallery__link" href="${url}">
+      <img
+        class="gallery__image"
+        src="${miniUrl}"
+        data-source="${url}"
+        alt="${alt}"
+      />
+    </a>`)
+    newItem.classList.add("gallery__item")
+    galleryEl.append(newItem)
+}
+//~===========================================
+
+
+galleryItems.forEach(photo => {
+    createGalleryItem(photo)
+})
+
+galleryEl.addEventListener("click", (event)=>{
+    event.preventDefault();
+    if(event.target.nodeName === "IMG"){
+
+      const currentPhoto = event.target
+      const showingPhoto = basicLightbox.create(`<img src="${currentPhoto.dataset.source}" alt="${currentPhoto.alt}" width="500">`)
+      currentShowingPhoto = showingPhoto
+      showingPhoto.show()
+    }
+  })
+
+document.addEventListener("keydown", (event) =>{
+  const showingPhoto = currentShowingPhoto
+  if(event.code === "Escape" ) showingPhoto.close()
+
+} )
+
+/*
+ *<li class="gallery__item">
+ *<a class="gallery__link" href="large-image.jpg">
+ *  <img
+ *    class="gallery__image"
+ *    src="small-image.jpg"
+ *    data-source="large-image.jpg"
+ *    alt="Image description"
+ *  />
+ *</a>
+ *</li>*/
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryEl = document.querySelector(".gallery");
 console.log(galleryItems);
+
+function createGalleryItem({
+  preview: miniUrl,
+  original: url,
+  description: alt,
+}) {
+  const newItem = document.createElement("li");
+  newItem.insertAdjacentHTML(
+    "afterbegin",
+    `<a class="gallery__link" href="${url}">
+    <img
+    class="gallery__image"
+    src="${miniUrl}"
+    alt="${alt}"
+    />
+    </a>`
+  );
+  newItem.classList.add("gallery__item");
+  galleryEl.append(newItem);
+}
+galleryItems.forEach((photo) => {
+  createGalleryItem(photo);
+});
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+galleryEl.addEventListener("click", (event) => event.preventDefault());
+
+/*
+* <li class="gallery__item">
+*    <a class="gallery__link" href="large-image.jpg">
+*       <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+*    </a>
+* </li>
+
+*/
